489d0fa6547a4acb2b876b130b9b5cf78c54c088
Marcio Carolino <marciocarolino@gmail.com>
2021-09-07 19:07:00 -0300

    adicionado dentro de database, uma pasta com sql, tb_user.sql

---
+++ src/database/sql/tb_user.sql
@@ -0,0 +1,11 @@
+create table desejo.tb_user(
+
+  idUser integer not null auto_increment primary key,
+  name varchar(255),
+  telephone varchar(20),
+  city varchar(50),
+  state varchar(4),
+  zipCode varchar(20),
+  user varchar(25),
+  password varchar(50)
+)
